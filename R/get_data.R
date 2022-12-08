fed60445d390a358e630e5d45320352270dcd925
Dennis Kostka <dennis@Denniss-MacBook-Pro.local>
2022-12-07 22:38:39 -0500

    Initial commit

---
+++ R/get_data.R
@@ -0,0 +1,148 @@
+
+#- THIS FILE MAKES DATA aenmd needs
+#- Currently using:
+#
+#  AnnotationHub / Ensembl v. 105 ("AH98047")
+
+FORCE_CREATE = FALSE #- set to TRUE to overwrite existing data
+
+prefix <- here::here("inst","extdata")
+
+datfiles <- c(  "env_ensdb_v105_exns_byTx_fil.rds",
+                "env_ensdb_v105_seqs_byTx_fil.rds",
+                "env_ensdb_v105_splc_byTx_fil.rds",
+                "env_ensdb_v105_setx_byTx_fil.rds",
+                "grl_ensdb_v105_splc_byTx_fil.rds",
+                "grl_ensdb_v105_exns_byTx_fil.rds",
+                "grl_ensdb_v105_trnscrpts_fil.rds")
+
+if((file.exists(paste(prefix, datfiles, sep="/")) |> all()) && (!FORCE_CREATE)){
+    
+    #- nothing to do
+
+} else { #- generate data.
+
+Hsapiens <- BSgenome.Hsapiens.UCSC.hg38::Hsapiens
+seqlevelsStyle(Hsapiens) <- 'NCBI'
+genome(Hsapiens) <- 'GRCh38' #- ugly, but otherwise the ensdbmerge down there does not work.
+                              #- also pretty sure this is corredt in that the ensembl annoations are  are also a newer patch 
+
+AH_VERSION <- 'AH98047'
+
+#- Get the data
+ah   <- AnnotationHub::AnnotationHub()
+edb  <- ah[[AH_VERSION]]
+
+#- COMPILE TRANSCRIPT SET
+#------------------------
+
+#- all protein-coding txs on standard chromosomes
+FilterList <- list(     ensembldb::SeqNameFilter(as.character(c(1:22,"X","Y","MT"))),
+                        ensembldb::TxBiotypeFilter("protein_coding"))
+txs <- ensembldb::transcripts(edb, filter = FilterList,
+                               columns = c( "tx_id_version","gene_id", "tx_support_level", "tx_is_canonical"))
+
+#- corresponding exon ranges
+all_exn_rng        <- ensembldb::cdsBy(edb, "tx", filter = ensembldb::TxIdFilter(names(txs)))   #- cds only
+all_tx_exn_rng     <- ensembldb::exonsBy(edb, "tx", filter = ensembldb::TxIdFilter(names(txs))) #- whole transcript
+#- these are exactly the same transcripts
+if(! all(names(all_exn_rng) == names(all_tx_exn_rng)) ) stop("Transcripts naming issue")
+txs                <- txs[names(all_exn_rng),]
+txs$num_exons_cds  <- S4Vectors::elementNROWS(all_exn_rng)
+txs$num_exons_txs  <- S4Vectors::elementNROWS(all_tx_exn_rng)
+#- sanity check
+if(! all(txs$num_exons_cds <= txs$num_exons_txs) ) stop("Transcripts error counting issue")
+
+#- tsl 1 transcripts plus
+#- recover 1-exon transcripts withou transcript support level info
+ind <- txs$tx_support_level == 1
+ind <- ind | ((txs$num_exons_txs == 1) & (is.na(txs$tx_support_level)))
+
+txs_used     <- txs[sort(which(ind))]
+exn_rng_used <- all_exn_rng[names(txs_used)]
+cds_used     <- BSgenome::getSeq(Hsapiens, exn_rng_used)
+
+#- FILTER CDS must be divisible by three
+#- some of these sequences are sort of incomplete
+#  in this case the cds is not divisible by three.
+#  we will exclude these but keep track.
+out <- which(width(exn_rng_used) |> lapply(sum) |> unlist() %% 3 != 0) |> sort()
+# length(out)
+# 927
+write.table(names(out) |> sort(), file="../inst/extdata/enst_length_excluded.txt",
+            quote = FALSE, row.names = FALSE, col.names = FALSE)
+txs_used     <- txs_used[ -out  ]
+exn_rng_used <- exn_rng_used[ -out  ]
+cds_used     <- cds_used[ -out  ]
+
+#- TODO:
+#- FILTER CDS must have at least 3 codons (start, content, stop)
+
+#- annotate splice regions based on:
+#  3bp into the exon and 8bp into the intron;
+
+#- exons (with utrs) ; but for ANY protein-coding transcript on standard chromosomes
+ex   <- ensembldb::exonsBy(edb, "tx", filter = FilterList, columns = c("exon_id", "tx_name"))
+ex_s <- resize(ex, width=3, fix = "start")
+ex_e <- resize(ex, width=3, fix = "end")
+sr_s <- resize(ex_s, width = 11, fix = "end")
+sr_e <- resize(ex_e, width = 11, fix = "start")
+exn_spl_rng <-  pc(sr_s,sr_e)  %>% sort()
+
+#- aggregate for all txs_used ALL overlapping splice regions
+ov <- GenomicRanges::findOverlaps(txs_used, exn_spl_rng)
+
+afu <- function(ind){
+	#tx_nme                <- names(txs_used)[ind]
+    sh                    <- GenomicRanges::subjectHits(ov)[which(GenomicRanges::queryHits(ov)==ind)]
+	res                   <- exn_spl_rng[sh] %>% unlist() %>% sort()
+	mcols(res)$tx_biotype <- NULL
+	return(res)
+}
+
+#- FIXME: slow, there needs to be a better way.
+spl_rng_used        <- sapply(seq_len(length(txs_used)), afu) %>% GenomicRanges::GRangesList()
+names(spl_rng_used) <- names(txs_used)
+
+#- NOTE
+#======
+#  It appears several orders of magnitude faster to
+#  look up GRanges in an environment compared with a GRangesList.
+#  Therefore we make these.
+#  Also, we pre-make all the reference sequences.
+#  Takes long to make, though.
+
+#- Empty envs
+exon_env   <- new.env(hash=TRUE, parent = emptyenv())
+cds_env    <- new.env(hash=TRUE, parent = emptyenv())
+splice_env <- new.env(hash=TRUE, parent = emptyenv())
+set_env    <- new.env(hash=TRUE, parent = emptyenv()) #- list of single exon transcripts
+
+#- Fill envs with content
+for( i in seq_len(length(txs_used))){
+        if(i %% 10 == 0) message( i )
+        txn               <- names(txs_used)[i]
+        exns              <- exn_rng_used[[txn]]
+        exon_env[[txn]]   <- exns
+        seq_ref_exns      <- cds_used[[txn]]
+        seq_ref_c         <- unlist(seq_ref_exns)
+        cds_env[[txn]]    <- seq_ref_c
+    	spl               <- spl_rng_used[[txn]]
+	splice_env[[txn]] <- spl
+	if(txs_used$num_exons_txs[i] == 1){
+		set_env[[txn]] <- TRUE #- true 1-exon transcripts
+	}
+}
+
+#- we put the environments into inst/extdata
+if(TRUE){
+    saveRDS(exon_env,     file = "../inst/extdata/env_ensdb_v105_exns_byTx_fil.rds")
+    saveRDS(cds_env,      file = "../inst/extdata/env_ensdb_v105_seqs_byTx_fil.rds")
+    saveRDS(splice_env,   file = "../inst/extdata/env_ensdb_v105_splc_byTx_fil.rds")
+    saveRDS(set_env,      file = "../inst/extdata/env_ensdb_v105_setx_byTx_fil.rds")
+    saveRDS(spl_rng_used, file = "../inst/extdata/grl_ensdb_v105_splc_byTx_fil.rds")
+    saveRDS(exn_rng_used, file = "../inst/extdata/grl_ensdb_v105_exns_byTx_fil.rds")
+    saveRDS(txs_used,     file = "../inst/extdata/grl_ensdb_v105_trnscrpts_fil.rds")
+}
+
+} #- end else
---
+++ R/zzz.R
@@ -0,0 +1,49 @@
+#- Load processed ENSEMBL annotation
+#-----------------------------------
+
+._EA_exn_env <- NULL #- exons we use
+._EA_cds_env <- NULL #- cds sequences we use
+._EA_spl_env <- NULL #- splice regions we use
+._EA_spl_grl <- NULL #- splice regions we use as grl
+._EA_exn_grl <- NULL #- exons we use as grl
+._EA_txs_grl <- NULL #- transcripts we use as grl
+._EA_snv_tri <- NULL #- SNVs that caust stop-gains in transcripts we use
+._EA_set_env <- NULL #- list of single exon transcripts ; TODO: make me a tri
+
+
+.onLoad <- function(libname, pkgname) {
+
+    #- load envs containing data we need
+    #-----------------------------------
+    prefix       <- system.file("extdata", package = "aenmd_data_ensdb_v105")
+
+    ._EA_exn_env <<- future::future({readRDS(paste0(prefix,'/','env_ensdb_v105_exns_byTx_fil.rds'))}, lazy = TRUE)
+    #environment(._EA_exn_env) <- asNamespace('aenmd')
+
+    ._EA_cds_env <<- future::future({readRDS(paste0(prefix,'/','env_ensdb_v105_seqs_byTx_fil.rds'))}, lazy = TRUE)
+    #environment(._EA_cds_env) <- asNamespace('aenmd')
+
+    ._EA_spl_env <<- future::future({readRDS(paste0(prefix,'/','env_ensdb_v105_splc_byTx_fil.rds'))}, lazy = TRUE)
+    #environment(._EA_spl_env) <- asNamespace('aenmd')
+
+    ._EA_set_env <<- future::future({readRDS(paste0(prefix,'/','env_ensdb_v105_setx_byTx_fil.rds'))}, lazy = TRUE)
+    #environment(._EA_set_env) <- asNamespace('aenmd')
+
+    ._EA_spl_grl <<- future::future({readRDS(paste0(prefix,'/','grl_ensdb_v105_splc_byTx_fil.rds'))}, lazy = TRUE)
+    #environment(._EA_spl_grl) <- asNamespace('aenmd')
+
+    ._EA_exn_grl <<- future::future({readRDS(paste0(prefix,'/','grl_ensdb_v105_exns_byTx_fil.rds'))}, lazy = TRUE)
+    #environment(._EA_exn_grl) <- asNamespace('aenmd')
+
+    ._EA_txs_grl <<- future::future({readRDS(paste0(prefix,'/','grl_ensdb_v105_trnscrpts_fil.rds'))}, lazy = TRUE)
+    #environment(._EA_txl_grl) <- asNamespace('aenmd')
+
+    ._EA_snv_tri <<- future::future({
+        m_keys <- readRDS(paste0(prefix,'/','tri-keys_ensdb_v105_fil_all-stop-making-snvs.rds'))
+        m_vals <- readRDS(paste0(prefix,'/','tri-vals_ensdb_v105_fil_all-stop-making-snvs.rds'))
+        m_trie <- triebeard::trie(keys = m_keys, values = m_vals)},
+        lazy = TRUE)
+    #environment(._EA_snv_tri) <- asNamespace('aenmd')
+}
+
+
